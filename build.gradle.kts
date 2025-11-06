7952bb38dacbfce4affffa4bda254a3258e5b18b
Jeffrey Liu <jeffreyliu8@users.noreply.github.com>
2025-11-05 22:02:08 -0800

    chore: Integrate Detekt for static code analysis (#1)
    This commit introduces the Detekt static analysis tool to the project to improve code quality.
    
    - Add the `detekt` Gradle plugin and its version to `libs.versions.toml`.
    - Apply the plugin to all projects in the root `build.gradle.kts`.
    - Configure Detekt to use a custom configuration file (`detekt/detekt-config.yml`) and automatically discover source sets.
    - Add a default `detekt-config.yml` with most documentation-related rules disabled.
    - Integrate the `detekt` task into the CI workflow (`android.yml`).
    - Update `README.md` with instructions for running Detekt.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.ktlint) apply false
+    alias(libs.plugins.detekt)
 }
 
 subprojects {
@@ -15,4 +16,30 @@ subprojects {
     configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
         debug.set(true)
     }
+}
+
+
+allprojects {
+    apply(plugin = "dev.detekt")
+    detekt {
+        buildUponDefaultConfig = true
+        config.setFrom(files("${rootProject.projectDir}/detekt/detekt-config.yml"))
+        source.setFrom(getDetektSourcePaths())
+    }
+}
+
+private fun getDetektSourcePaths(): List<File> {
+    val sourceDirs = mutableListOf<File>()
+
+    subprojects.forEach {
+        sourceDirs.add(file("${it.projectDir}/src/commonMain/kotlin"))
+        sourceDirs.add(file("${it.projectDir}/src/androidMain/kotlin"))
+        sourceDirs.add(file("${it.projectDir}/src/iosMain/kotlin"))
+        sourceDirs.add(file("${it.projectDir}/src/jsMain/kotlin"))
+        sourceDirs.add(file("${it.projectDir}/src/jvmMain/kotlin"))
+        sourceDirs.add(file("${it.projectDir}/src/wasmJsMain/kotlin"))
+        sourceDirs.add(file("${it.projectDir}/src/webMain/kotlin"))
+    }
+
+    return sourceDirs.filter { it.exists() }
 }
